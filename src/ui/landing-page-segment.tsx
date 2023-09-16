5b209d0e22378cefadad3f56092cb51fe27ebe05
Nika870 <nikagvenetadze870@gmail.com>
2023-09-16 15:13:03 +0400

    rewrote the remaining three components in tailwind

--- src/ui/landing-page-segment.tsx
+++ src/ui/landing-page-segment.tsx
@@ -1,4 +1,3 @@
-import styles from "./landing-page-segment.module.css";
 import { NextImage } from "@/types/nextImage";
 
 export default function LandingPageSegment({
@@ -16,16 +15,20 @@ export default function LandingPageSegment({
 }) {
 	return (
 		<section
-			className={`${styles.section} 
-				${backgroundColor == "primary" ? styles.greenBackground : styles.whiteBackground} 
-				${textAlign === "left" ? "" : styles.reverse}`}
+			className={` 
+				${backgroundColor === "primary" ? "bg-green text-white" : "bg-white text-black"} 
+				${textAlign === "left" ? "" : "flex-row-reverse"} 
+				flex justify-around items-center w-full min-h-[590px] max-[820px]:flex-col max-[820px]:my-[50]`}
 		>
-			<div className={`${styles.messageDiv}`}>
-				<h2 className={styles.sectionMainMessage}>{title}</h2>
-				<p className={styles.description}>{description}</p>
+			<div className="max-[820px]:text-center">
+				<h2 className="mb-[15px] text-5xl">{title}</h2>
+				<p className="mb-[25px] text-lg max-[640px]:text-md max-[640px]:leading-5 
+				max-[820px]:max-w-[550px] max-[640px]:max-w-[350px] mx-auto">
+					{description}
+				</p>
 				<button
 					type="button"
-					className={backgroundColor == "primary" ? "buttonPrimary" : "buttonSecondary"}
+					className={`${backgroundColor === "primary" ? "buttonPrimary" : "buttonSecondary"}`}
 				>
 					Learn More
 				</button>
