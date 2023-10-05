7031c9235c1318776ac0acefffc189fff3e49925
Nika870 <nikagvenetadze870@gmail.com>
2023-10-05 15:00:07 +0400

    refactored error-message ui component

--- src/ui/error-message.tsx
+++ src/ui/error-message.tsx
@@ -1,18 +1,21 @@
+import { AxiosError } from "axios";
 import { Watch } from "react-loader-spinner";
 
 interface ErrorTryingAgainMessageProps {
-	children?: string;
+	AxiosError?: AxiosError;
 }
 
-const ErrorTryingAgainMessage = (props: ErrorTryingAgainMessageProps) => {
+const ErrorMessage = (props: ErrorTryingAgainMessageProps) => {
 	return (
 		<div className="mx-auto flex justify-center items-center">
 			<p className="text-center my-20 text-lg text-red mr-2">
-				{props.children ? props.children : "Error fetching data, trying again..."}
+				{props.AxiosError
+					? props.AxiosError.message + ", trying again."
+					: "Error fetching data, trying again..."}
 			</p>
 			<Watch color="red" height="30px" width="30px"></Watch>
 		</div>
 	);
 };
 
-export default ErrorTryingAgainMessage;
+export default ErrorMessage;
